c91f61c6b0e2932b4b74dfb53f3079c317d7e79e
brazafonso <61985783+brazafonso@users.noreply.github.com>
2023-04-15 16:50:00 +0100

    links diretos para relacoes genealogicas

--- tp1/treat_dataset.py
+++ tp1/treat_dataset.py
@@ -9,6 +9,35 @@ import traceback
 path = os.getcwd()
 
 
+
+def get_info_scopecontent(fields):
+    '''Get info from groups of scopecontent field'''
+    fil = []
+    if fields[1]:
+        fil = [x.strip() for x in fields[1].split(' e ')]
+    birth = 'null'
+    if fields[2]:
+        birth = fields[2]
+    cur_concelho = 'null'
+    if fields[3]:
+        cur_concelho = fields[3]
+    cur_district = 'null'  
+    if fields[4]:
+        cur_district = fields[4]
+    other = fields[7]
+    return [fil, birth, cur_concelho,cur_district,other]
+    
+
+
+
+
+
+
+
+
+
+
+
 dataset_file = open(f'{path}/data/PT-UM-ADB-DIO-MAB-006.CSV','r')
 
 # Load dataset
@@ -30,6 +59,7 @@ for col in df.columns:
 ds_info = df.iloc[0]
 # print(ds_info)
 df.drop(index=0,axis=0,inplace=True)
+df.reset_index(drop=True, inplace=True)
 
 # Drop columns because they have redundant information (available from other columns or from ds_info)
 df.drop(['DescriptionLevel','UnitTitleType','CompleteUnitId','LangMaterial','AllowExtentsInference','AllowTextualContentInference','RepositoryCode','CountryCode','Repository','ApplySelectionTable','Highlighted'],axis=1,inplace=True)
@@ -46,6 +76,34 @@ pd.set_option('display.max_columns', None)
 df['UnitTitle'] = df['UnitTitle'].str.replace(r'.*de genere de\s+((\w+\b\s*)+)',r'\1', regex=True)
 df['UnitTitle'] = df['UnitTitle'].map(lambda x: x.split(' e '),na_action='ignore')
 
+
+
+# Make new field to directly link relations if possible
+relations_id = []
+count = 0
+for i in df.index:
+    e = df.iloc[i]
+    try:
+        id = e['ID']
+        people = e['UnitTitle']
+        rel = []              
+        relations = e['RelatedMaterial']
+        if relations != 'null':
+            groups = re.findall(r',([^,]+)\.\s*Proc\.(\d+)',relations)
+            if groups:
+                j = 0
+                while j < len(groups):
+                    rel.append({groups[j][1]:groups[j][0]})
+                    j += 1
+                count+=1
+        relations_id.append(rel)
+    except Exception:
+        print(e)
+        print(traceback.format_exc())
+        exit(1)
+
+
+
 # Get info from ScopeContent
 filiations = []
 birthplace = []
@@ -53,28 +111,40 @@ current_concelho = []
 current_district = []
 
 re_scopecontent= r'(.+:(.+)\..+)?residente em\s*(.+),.*concelho de (.+?)( e distrito\s*(\(.+?\))?\s*(.+?))?\.(.+)?'
-people_id_dic = {}
+
 for i in df.index:
-    e = df.iloc[i-1]
+    e = df.iloc[i]
     try:
         id = e['ID']
         people = e['UnitTitle']
+        note = e['Note']
         scopecontent = e['ScopeContent']
-        scopecontent = re.sub(re_scopecontent,r'\2|\3|\4|\7',scopecontent)
-        fields = scopecontent.split('|')
-        fil = [x.strip() for x in fields[0].split(' e ')]
-        birth = fields[1]
-        cur_concelho = fields[2]
-        cur_district = fields[3]
-        for p in people:
-            if not p in people_id_dic:
-                people_id_dic[p] = {}
-            people_id_dic[p][id] = fil
+        fields = re.search(re_scopecontent,scopecontent).groups()
+        fil,birth,cur_concelho,cur_district,other = get_info_scopecontent(fields)
+        # Other may have aditional relation information
+        if other:
+            rel = re.findall(r',([^,]+)\.\s*Proc\.(\d+)',other)
+            if rel:
+                j = 0
+                while j < len(rel):
+                    relations_id[i].append({rel[j][1]:rel[j][0]})
+                    j += 1
+                count+=1
+        if note != 'null':
+            rel = re.findall(r',([^,]+)\.\s*Proc\.(\d+)',note)
+            if rel:
+                j = 0
+                while j < len(rel):
+                    relations_id[i].append({rel[j][1]:rel[j][0]})
+                    j += 1
+                count+=1
+        
         filiations.append(fil)
         birthplace.append(birth)
         current_concelho.append(cur_concelho)
         current_district.append(cur_district)
     except Exception as error:
+        print(rel)
         print(e)
         print(traceback.format_exc())
         exit(1)
@@ -85,40 +155,10 @@ df['current_concelho'] = current_concelho
 df['current_district'] = current_district
 df.drop('ScopeContent',axis=1,inplace=True)
 
-# Make new field to directly link relations if possible
-# relations_id = []
+print('count',count)
 
-# for i in df.index:
-#     e = df.iloc[i-1]
-#     try:
-#         id = e['ID']
-#         print(id)
-#         people = e['UnitTitle']
-#         fil = e['filiations']
-#         rel = set()
-#         # for each person of the doc
-#         for p in people:
-#             found = False
-#             # for each afiliation
-#             for f in fil:
-#                 if f in people_id_dic:
-#                     # check if the afiliated afiliates to the person
-#                     for key,filiation in people_id_dic[f].items():
-#                         if p in filiations:
-#                             rel.add(key)
-#                             print('----------------found------------------')
-#                             found = True
-#                             break
-#                     if found:
-#                         break
-                            
-#         relations_id.append(rel)
-#     except Exception:
-#         print(e)
-#         print(traceback.format_exc())
-#         exit(1)
 
-# df['relations_id'] = relations_id
+df['relations_id'] = relations_id
 
 
 
